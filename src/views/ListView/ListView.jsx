ce0ac1081da3ad70df3a847ea91bc4547e22fec2
Irina-Koksharova <biodgerelo@ukr.net>
2020-12-29 04:36:54 +0200

    Изменяет стили компонента LisView

--- src/views/ListView/ListView.jsx
+++ src/views/ListView/ListView.jsx
@@ -4,6 +4,7 @@ import { fetchSelectedMovies } from '../../services/api-movies';
 
 const ListView = ({ movies }) => {
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const imageURL = 'https://image.tmdb.org/t/p/w400';
 
   const getSelectedMovie = e => {
     e.preventDefault();
@@ -13,11 +14,22 @@ const ListView = ({ movies }) => {
   };
 
   return (
-    <ul>
-      {movies.map(({ id, title }) => (
-        <li key={id}>
+    <ul className={s.list}>
+      {movies.map(({ id, title, poster_path, release_date }) => (
+        <li className={s.item} key={id}>
           <a className={s.link} href="/" id={id} onClick={getSelectedMovie}>
-            {title}
+            <div className={s.containerImage}>
+              <img
+                className={s.image}
+                src={imageURL + poster_path}
+                alt={title}
+              />
+            </div>
+            <div className={s.containerTitle}>
+              <p className={s.title}>
+                {title + ` (${release_date.slice(0, 4)})`}
+              </p>
+            </div>
           </a>
         </li>
       ))}
